ca85e09d068ed43c05e35cb384e2a6f35ca26bd9
David <d.sommer@weptun.de>
2018-12-14 14:37:37 +0100

    Updated opening of image

--- downloadFiles.bat
+++ downloadFiles.bat
@@ -6,11 +6,13 @@ set lonsteps=4
 set /A lonEnd=%lon%+(%lonstep%*%lonsteps%)
 
 set lat=57034
-set latstep=100
-set latsteps=4
+set latstep=1000
+set latsteps=1
 set /A latEnd=%lat%+(%latstep%*%latsteps%)
 
 set zoom=17.0
 for /l %%l in (%lat%, %latstep%, %latEnd%) do (curl https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/11.%%l,48.%lon%,%zoom%,0,0/1280x1280@2x?access_token=%MAPBOX_ACCESS_KEY% --output %%l.png)
+
 magick convert *.png +append result.png
-"C:\Program Files\IrfanView\i_view64.exe" result.png
+
+start /b "C:\Program Files\IrfanView\i_view64.exe" result.png

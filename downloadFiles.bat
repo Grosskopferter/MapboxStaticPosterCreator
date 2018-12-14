8e3f2fdaa7e71d2de995232b453fe2581b012a30
David <d.sommer@weptun.de>
2018-12-14 16:43:48 +0100

    Creating grid image

--- downloadFiles.bat
+++ downloadFiles.bat
@@ -1,8 +1,8 @@
 del *.png
-set lat=57034
-set lon=12867
-set lonstep=100
-set lonsteps=4
+
+set lon=128670
+set lonstep=-6867
+set lonsteps=1
 set /A lonEnd=%lon%+(%lonstep%*%lonsteps%)
 
 set lat=570340
@@ -11,8 +11,11 @@ set latsteps=1
 set /A latEnd=%lat%+(%latstep%*%latsteps%)
 
 set zoom=17.0
-for /l %%l in (%lat%, %latstep%, %latEnd%) do (curl https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/11.%%l,48.%lon%,%zoom%,0,0/1280x1280@2x?access_token=%MAPBOX_ACCESS_KEY% --output %%l.png)
+for /l %%o in (%lon%, %lonstep%, %lonEnd%) do for /l %%a in (%lat%, %latstep%, %latEnd%) do (curl https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/11.%%a,48.%%o,%zoom%,0,0/1280x1280@2x?access_token=%MAPBOX_ACCESS_KEY% --output %%o-%%a.png)
 
-magick convert *.png +append result.png
+for /l %%o in (%lon%, %lonstep%, %lonEnd%) do (magick convert %%o-*.png +append vert-%%o.png)
+
+magick convert vert-*.png -append result.png
+
 
 start /b "C:\Program Files\IrfanView\i_view64.exe" result.png

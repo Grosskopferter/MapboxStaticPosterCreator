542ff13ade1f72460e3201d58189ddcea4ff7b4b
David Sommer <d.sommer@weptun.de>
2018-12-17 07:29:56 +0100

    Added custom style for map

--- downloadFiles.bat
+++ downloadFiles.bat
@@ -11,7 +11,7 @@ set latsteps=1
 set /A latEnd=%lat%+(%latstep%*%latsteps%)
 
 set zoom=17.0
-for /l %%o in (%lon%, %lonstep%, %lonEnd%) do for /l %%a in (%lat%, %latstep%, %latEnd%) do (curl https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/11.%%a,48.%%o,%zoom%,0,0/1280x1280@2x?access_token=%MAPBOX_ACCESS_KEY% --output %%o-%%a.png)
+for /l %%o in (%lon%, %lonstep%, %lonEnd%) do for /l %%a in (%lat%, %latstep%, %latEnd%) do (curl https://api.mapbox.com/styles/v1/grant/cjppy2mdv0pvn2sqjsliywuig/static/11.%%a,48.%%o,%zoom%,0,0/1280x1280@2x?access_token=%MAPBOX_ACCESS_KEY% --output %%o-%%a.png)
 
 for /l %%o in (%lon%, %lonstep%, %lonEnd%) do (magick convert %%o-*.png +append vert-%%o.png)
 
